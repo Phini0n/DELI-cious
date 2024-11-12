8a35d7482ec0c7dda97b2d2c390285c498413da3
Phini0n <patrayowilson@gmail.com>
2024-11-12 15:09:35 -0500

    Added handleOrcerScreen

--- src/main/java/com/pluralsight/control/UserInterface.java
+++ src/main/java/com/pluralsight/control/UserInterface.java
@@ -29,6 +29,7 @@ public class UserInterface {
     private void handleDisplay() {
         boolean firstRun = true;
         menuState = MenuState.HOME_SCREEN;
+
         while (true) {
             if(firstRun) {
                 System.out.println("Hello, welcome to the DELI-cious deli program! \nHow can we help you?\n");
@@ -37,25 +38,54 @@ public class UserInterface {
 
             // Printing out the Home Screen menu state and its options.
             System.out.println(menuState.getTitle());
-            System.out.print(menuState.getOptionsList());
+            System.out.println(menuState.getOptionsList());
+            System.out.print("Enter: ");
 
             int choice = scanner.nextInt();
             switch (menuState) {
                 case HOME_SCREEN -> handleHomeScreen(choice);
+                case ORDER_SCREEN -> handleOrderScreen(choice);
             }
-
         }
     }
 
     private void handleHomeScreen(int choice) {
         switch (choice) {
-            case 1:
+            case 1: // New Order
+                menuState = MenuState.ORDER_SCREEN;
                 break;
-            case 2:
-                break;
-            case 0:
+            case 0: // Exit
                 System.out.println("Thank you! Exiting program. . .");
                 System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid entry.");
+        }
+    }
+
+    private void handleOrderScreen(int choice) {
+        switch (choice) {
+            case 1: // Add Sandwich
+                break;
+            case 2: // Add Drink
+                break;
+            case 3: // Add Chips
+                break;
+            case 4: // Checkout
+                break;
+            case 0: // Cancel Order
+                System.out.println("Are you sure you want to cancel your order? (y/n)");
+                System.out.print("Enter: ");
+                scanner.nextLine(); // Clearing buffer
+                switch (scanner.nextLine().toLowerCase()) {
+                    case "y":
+                        System.out.println("Returning to order menu . . .");
+                        menuState = MenuState.HOME_SCREEN;
+                        break;
+                    case "n":
+                        System.out.println("Your order may resume.");
+                        break;
+                }
                 break;
             default:
                 System.out.println("Invalid entry.");
--- src/main/java/com/pluralsight/model/userinterface/MenuState.java
+++ src/main/java/com/pluralsight/model/userinterface/MenuState.java
@@ -3,7 +3,7 @@ package com.pluralsight.model.userinterface;
 public enum MenuState {
     HOME_SCREEN("Home Screen", new String[] {"New Order", "Exit"}),
     ORDER_SCREEN("Order Screen", new String[] {"Add Signature Sandwich", "Add New Sandwich",
-            "Add Drink", "Add Chips", "Checkout"});
+            "Add Drink", "Add Chips", "Checkout", "Cancel Order"});
 
     private final String title;
     private final String[] options;
@@ -24,7 +24,7 @@ public enum MenuState {
     public String getOptionsList() {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < this.getOptions().length; i++) {
-            // If we are reaching the last option/index, it becomes zero. Else, add 1.
+            // If we are reaching the last option/index, it becomes zero. Else, add 1 to display it.
             int displayIndex = (i == this.getOptions().length - 1) ? 0 : i + 1;
             stringBuilder.append(displayIndex).append(") ").append(this.getOptions()[i]);
             if(!(i == this.getOptions().length)) { stringBuilder.append(System.lineSeparator()); }
