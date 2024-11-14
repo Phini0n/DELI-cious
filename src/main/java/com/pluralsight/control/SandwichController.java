1df9c4969462a67e4bfc0cd87da23a07a259b7e7
Phini0n <patrayowilson@gmail.com>
2024-11-14 10:12:34 -0500

    Split sandwich building process into individual methods.

--- src/main/java/com/pluralsight/control/SandwichController.java
+++ src/main/java/com/pluralsight/control/SandwichController.java
@@ -2,7 +2,6 @@ package com.pluralsight.control;
 
 import com.pluralsight.model.MenuState;
 import com.pluralsight.model.Size;
-import com.pluralsight.model.menuitem.Drink;
 import com.pluralsight.model.menuitem.sandwich.Bread;
 import com.pluralsight.model.menuitem.sandwich.Sandwich;
 import com.pluralsight.model.menuitem.sandwich.toppings.Topping;
@@ -12,70 +11,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SandwichController {
-    private Display display;
+    private Display display = new Display();
     private MenuState menuState;
 
     // Sandwich Variables
     Sandwich sandwich;
     private Size sandwichSize;
     private boolean isToasted;
+    private boolean isCancelled; // Determines if the initalize process will continue, or not.
     private Bread bread;
     private List<Topping> toppings = new ArrayList<Topping>();
+    private List<Object> cart = new ArrayList<>();
 
-    private void init() {
-        display = new Display();
+    // Using Custom Sandwich
+    private void initializeSandwich() {
+        isCancelled = false;
+        processSizeRequest();
+        processIsToastedRequest();
+        processBreadRequest();
+        if (isCancelled) {
+            display.showMessage("Sandwich process was cancelled. Returning to Order menu.");
+        }
     }
 
-    public void display() {
-        init();
-
-        handleDisplay();
+    // Using Signature Sandwich
+    private void initializeSandwich(Sandwich sandwich) {
+        // TODO: Implement this.
     }
 
-    private void handleDisplay() {
-
-    }
-
-    public Sandwich createSandwich() {
-        // Bread
+    private void handleSandwichScreen() {
         display.showMessageLine("""
-                Enter your bread (White, Wheat, Rye, Wrap):
+                Sandwich Builder
+                1) Bread
+                2) Toppings
                 
                 Enter:\s
                 """);
-        try {
-            Bread.fromString(display.getUserString());
-        } catch (Exception e) {
-            display.showMessageLine("\nError " + e + " occurred with your input. Returning to order menu.");
-            return null;
-        }
+    }
 
-        // Size
-        display.showMessageLine("""
-                Enter your size (Small, Medium, Large):
-                
-                Enter:\s
-                """);
-        try {
-            Size.fromString(display.getUserString());
-        } catch (Exception e) {
-            display.showMessageLine("\nError "  + e + " occurred with your input. Returning to order menu.");
-        }
-
-        // isToasted
-        display.showMessage("Would you like your bread toasted? (Y/N)");
-        if (display.getUserString().equalsIgnoreCase("Y")) {
-            isToasted = true;
-        } else if (display.getUserString().equalsIgnoreCase("N")) {
-            isToasted = false;
-        } else {
-            display.showMessageLine("Invalid input, returning to order menu.");
-            return null;
-        }
-
+    public Sandwich handleToppingsScreen() {
         // Toppings
         display.showMessageLine("""
-                Select your toppings:
+                Toppings Builder
                 1) Meat
                 2) Cheese
                 3) Other toppings
@@ -83,7 +60,88 @@ public class SandwichController {
                 
                 Enter:\s
                 """);
+        return null;
+    }
 
+    private void processSizeRequest() {
+        if (!isCancelled) {
+            display.showMessageLine("""
+                Enter your size (Small, Medium, Large):
+                Enter 0 to stop Sandwich Building.
+                
+                """);
+            while (true) {
+                display.showMessage("Enter: ");
+                try {
+                    String input = display.getUserString();
+                    if (input.equals("0")) {
+                        display.showMessage("Exiting Sandwich Building interface. . . ");
+                        isCancelled = true;
+                        return;
+                    }
+                    sandwichSize = Size.fromString(input);
+                } catch (Exception e) {
+                    display.showMessageLine("\nError "  + e + " occurred with your input. " +
+                            "Please try again, or enter 0 to exit.");
+                }
+            }
+        }
+    }
+
+    private void processIsToastedRequest() {
+        if (!isCancelled) {
+            display.showMessage("""
+                Would you like your bread toasted? (Y/N):
+                Enter 0 to stop Sandwich Building.
+                
+                """);
+            while (true) {
+                display.showMessage("Enter: ");
+                String input = display.getUserString();
+                switch (input.toLowerCase()) {
+                    case "y":
+                        isToasted = true;
+                        break;
+                    case "n":
+                        isToasted = false;
+                        break;
+                    case "0":
+                        display.showMessage("Exiting Sandwich Building interface. . . ");
+                        isCancelled = true;
+                        return;
+                    default:
+                        display.showMessageLine("Invalid input. Please try again, or enter 0 to exit.");
+                }
+            }
+        }
+    }
+
+    private void processBreadRequest() {
+        if (!isCancelled) {
+            display.showMessageLine("""
+                Enter your bread (White, Wheat, Rye, Wrap):
+                Enter 0 to stop Sandwich Building.
+                
+                """);
+            while (true) {
+                display.showMessage("Enter: ");
+                try {
+                    String input = display.getUserString();
+                    if (input.equals("0")) {
+                        display.showMessage("Exiting Sandwich Building interface. . . ");
+                        isCancelled = true;
+                        return;
+                    }
+                    bread = Bread.fromString(input);
+                } catch (Exception e) {
+                    display.showMessageLine("\nError "  + e + " occurred with your input. Please try again, or enter 0 to exit.");
+                }
+            }
+        }
+    }
+
+    public String showCurrentSandwich(ArrayList<Object> cart) {
+        return "";
     }
 
 //    public Sandwich createSandwich(Sandwich sandwich) {
